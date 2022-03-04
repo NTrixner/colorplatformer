05487b98a6b977946249d9fc6860cf24ae9b16cf
ntrixner <n.trixner@gmx.at>
2022-03-04 22:20:10 +0100

    Create Painting Shader

--- Assets/Scripts/PaintReceiver.cs
+++ Assets/Scripts/PaintReceiver.cs
@@ -1,9 +1,43 @@
+using System;
+using ColorPlatformer;
 using UnityEngine;
 
 public class PaintReceiver : MonoBehaviour
 {
+    private const int TEXTURE_SIZE = 1024;
+
+    public RenderTexture renderTexture;
+
+    public RenderTexture paintTexture;
+    
+    public Renderer renderer;
+
+    private int maskTextureID = Shader.PropertyToID("_MaskTexture");
+    private PaintManager paintManager;
+
+    private void Start()
+    {
+        paintManager = FindObjectOfType<PaintManager>();
+        renderTexture = new RenderTexture(TEXTURE_SIZE, TEXTURE_SIZE, 0);
+        renderTexture.filterMode = FilterMode.Bilinear;
+        renderTexture.name = "Mask";
+
+        paintTexture = new RenderTexture(TEXTURE_SIZE, TEXTURE_SIZE, 0);
+        paintTexture.filterMode = FilterMode.Bilinear;
+        paintTexture.name = "Paint";
+        
+        renderer = GetComponent<Renderer>();
+        renderer.material.SetTexture(maskTextureID, renderTexture);
+        
+        paintManager.initTextures(this);
+    }
+
     public void ReceivePaint(Vector3 point, Vector3 normal)
     {
-        Debug.Log(name + " received paint at " + point + " with normals " + normal);
+        paintManager.paint(this, point, 1f, 0.5f, 1f, Color.red);
+    }
+    
+    void OnDisable(){
+        renderTexture.Release();
     }
 }
