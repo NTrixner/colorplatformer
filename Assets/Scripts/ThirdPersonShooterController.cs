da563d3568077ca465d8878a577621dc44e8421a
ntrixner <n.trixner@gmx.at>
2022-03-05 10:20:04 +0100

    Hide reticle if not aiming

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using Cinemachine;
 using Movement;
+using UnityEngine.UI;
 
 public class ThirdPersonShooterController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
     [SerializeField] private float normalCameraSensitivity = 1f;
     [SerializeField] private float aimSensitivity = 0.5f;
+    [SerializeField] private Image reticle;
     private Inputs inputs;
     private ThirdPersonController thirdPersonController;
 
@@ -21,11 +23,13 @@ public class ThirdPersonShooterController : MonoBehaviour
         if (inputs.aim)
         {
             aimVirtualCamera.gameObject.SetActive(true);
+            reticle.gameObject.SetActive(true);
             thirdPersonController.SetCameraSensitivity(aimSensitivity);
         }
         else
         {
             aimVirtualCamera.gameObject.SetActive(false);
+            reticle.gameObject.SetActive(false);
             thirdPersonController.SetCameraSensitivity(normalCameraSensitivity);
         }
 
